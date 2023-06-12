afbce6b248f6f568dca22f4b62197e9fcdd5ec5d
Jorge Perez <info@jperez.ca>
2023-06-12 14:00:38 -0600

    projects updated

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,19 +1,51 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
+    title: 'Developer Portfolio',
+    description: `This very site that you're on. Created with Next.js & Tailwind.`,
     imgSrc: '/static/images/google.png',
     href: 'https://www.google.com',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
+    title: 'Main Personal Site - Ghost CMS',
+    description: `My main website for content creation, blogging, photo gallery and newsletter hub for my filmmaking & photography projects. Created with Ghost CMS, Node.js & hosted in Digital Ocean.`,
+    imgSrc: '/static/images/projects/ghost-cms.png',
     href: '/blog/the-time-machine',
+  },
+  {
+    title: 'Gumroad Digital Product - Solopreneur Template',
+    description: `A minimalist personal website template. Created with Bootstrap 5.`,
+    imgSrc: '/static/images/projects/solopreneur.jpg',
+    href: '/blog/the-time-machine',
+  },
+  {
+    title: 'Gumroad Digital Product - CardLink Template',
+    description: `A modular "Card-List" view template for your social media links. Created with Bootstrap 5.`,
+    imgSrc: '/static/images/projects/cardlink.jpg',
+    href: '/blog/the-time-machine',
+  },
+  {
+    title: 'My First Gumroad Digital Product - TreeLink Template',
+    description: `A modular "Link in bio" template for your social media links. Created with Bootstrap 5.`,
+    imgSrc: '/static/images/projects/treelink.jpg',
+    href: '/blog/the-time-machine',
+  },
+  {
+    title: 'Most Basic Website Ever',
+    description: `This one sucks, not gonna lie. This was the first attempt to create a personal website, an online photo gallery for my photographs. Created with Bootstrap.`,
+    imgSrc: '/static/images/projects/folio-thumb.png',
+    href: '/blog/the-time-machine',
+  },
+  {
+    title: 'TaskMaster - Flutter App',
+    description: `Ported my working iOS app to flutter. A simple To-do list, works on both iOS & Android phones. Created with Flutter & Dart.`,
+    imgSrc: '/static/images/projects/flutter-app.jpg',
+    href: '/',
+  },
+  {
+    title: 'TaskMaster - iOS App',
+    description: `Designed & developed a To-do List iOS App. Implemented with SwiftUI & Core Data. Compatible with dark mode as well. Works in the XCode iPhone emulator`,
+    imgSrc: '/static/images/projects/ios-app.jpg',
+    href: '/',
   },
 ]
 
